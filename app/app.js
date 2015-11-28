9e82f553ab4d24697c2d5f743158baaeeb89f0b5
TommyCakes <tomhwarhurst@aol.com>
2015-11-28 18:12:24 +0000

    Started home page added angular

---
+++ app/app.js
@@ -0,0 +1,58 @@
+// $(document).ready(function(){
+//    var scroll_start = 0;
+//    var startchange = $('#top');
+//    var offset = startchange.offset();
+//     if (startchange.length){
+//    $(document).scroll(function() {
+//       scroll_start = $(this).scrollTop();
+//       if(scroll_start > offset.top) {
+//           $(".navbar-default").css('background-color', 'rgba(0, 0, 0, 0.5)');
+//        }
+//    });
+//     }
+// });
+// $(document).ready(function(){
+//    var scroll_start = 0;
+//    var startchange = $('.pic-one');
+//    var offset = startchange.offset();
+//     if (startchange.length){
+//    $(document).scroll(function() {
+//       scroll_start = $(this).scrollTop();
+//       if(scroll_start > offset.top) {
+//           $(".navbar-default").css('background-color', 'black');
+//        }
+//    });
+//     }
+// });
+
+
+// angular.module('contactsSearch', [])
+//   .controller('SearchCtrl', function() {
+//      this.friends =
+//       [{name:'John', phone:'555-1276'},
+//       {name:'Mary', phone:'800-BIG-MARY'},
+//       {name:'Mike', phone:'555-4321'},
+//       {name:'Adam', phone:'555-5678'},
+//       {name:'Julie', phone:'555-8765'},
+//       {name:'Juliette', phone:'555-5678'},
+//       {name:'Reggie', phone:'555-5678'},
+//       {name:'Ali', phone:'555-5678'},
+//       {name:'Juliette', phone:'555-5678'},
+//       {name:'Tom', phone:'555-5678'},
+//       {name:'Phillip', phone:'555-5678'},
+//       {name:'Pauline', phone:'555-5678'},
+//       {name:'Jeremy', phone:'555-5678'},
+//       {name:'Sue', phone:'555-5678'},
+//       {name:'Dave', phone:'555-5678'},
+//       {name:'Bob', phone:'555-5678'},
+//       {name:'Juliette', phone:'555-5678'},
+//       {name:'Sarah', phone:'555-5678'},
+//       {name:'Rio', phone:'555-5678'},
+//       {name:'Dennis', phone:'555-5678'},
+//       {name:'Ralph', phone:'555-5678'},
+//       {name:'JD', phone:'555-5678'},
+//       {name:'Robert', phone:'555-5678'},
+//       {name:'Julian', phone:'555-5678'},
+//       {name:'Lunah', phone:'555-5678'},
+//     ];
+//   })
